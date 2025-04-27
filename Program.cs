89afea6d8922dbd801f6dca47bd3802efa3503e3
brendonwanderlust <brendon.wanderlust@gmail.com>
2025-04-27 11:13:49 -0400

    added default policy

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-var corsOriginsPolicy = "WanderWalletCorsOriginsPolicy";
+var builder = WebApplication.CreateSlimBuilder(args); 
 var allowedOrigins = new string[] { "https://localhost", "https://localhost:8100", "http://localhost:8100", "capacitor://localhost" };
 var headers = new string[] { "Origin", "Content-Length", "Content-Type", "Authorization", "Access-Control-Allow-Origin" };
 
@@ -9,20 +8,20 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(corsOriginsPolicy, policy =>
-    { 
-        policy
-            .WithOrigins(allowedOrigins)
-            .WithHeaders(headers); 
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(allowedOrigins);
+        policy.WithHeaders(headers); 
     });
 });
 
 var app = builder.Build();
 
 app.UseRouting();
-app.UseCors(corsOriginsPolicy);
+app.UseCors();
 
 var chatApi = app.MapGroup("/chat");
 chatApi.MapGet("/", () => "Hello, this is your wander wallet travel buddy. How can I help you today?");
