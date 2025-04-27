41f9758290752c60b5d6a2a71c5a47c9ad5bf29b
brendonwanderlust <brendon.wanderlust@gmail.com>
2025-04-27 09:26:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build(); 
+
+var todosApi = app.MapGroup("/chat");
+todosApi.MapGet("/", () => "Hello, this is your wander wallet travel buddy. How can I help you today?"); 
+app.Run();
+
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
