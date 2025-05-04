d061534b3209794b11a8a50ff74d6fc1ebc5e0eb
brendonwanderlust <brendon.wanderlust@gmail.com>
2025-05-04 15:32:19 -0400

    added middleware :\

--- Program.cs
+++ Program.cs
@@ -27,21 +27,21 @@ var app = builder.Build();
 
 app.UseCors();
 
-//app.Use(async (context, next) =>
-//{
-//    // Handle preflight requests for SSE
-//    if (context.Request.Method == "OPTIONS")
-//    {
-//        context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers["Origin"].ToString());
-//        context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//        context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
-//        context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-//        context.Response.StatusCode = 200;
-//        return;
-//    }
+app.Use(async (context, next) =>
+{
+    // Handle preflight requests for SSE
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers["Origin"].ToString());
+        context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+        context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+        context.Response.StatusCode = 200;
+        return;
+    }
 
-//    await next();
-//});
+    await next();
+});
 
 app.UseRouting();
 
