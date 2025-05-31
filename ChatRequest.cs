e2f018748bafebb30b308e626c81046f29cdf059
brendonwanderlust <brendon.wanderlust@gmail.com>
2025-05-31 13:51:12 -0400

    added Json attriburtes to request

--- ChatRequest.cs
+++ ChatRequest.cs
@@ -1,18 +1,35 @@
-﻿namespace wander_wallet_chat
+﻿using System.Text.Json.Serialization;
+
+namespace wander_wallet_chat
 {
     public class ChatRequest
     {
+        [JsonPropertyName("userId")]
         public string? UserId { get; set; }
+
+        [JsonPropertyName("message")]
         public string Message { get; set; } = string.Empty;
+
+        [JsonPropertyName("latitude")]
         public decimal Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
         public decimal Longitude { get; set; }
+
+        [JsonPropertyName("measurementSystem")]
         public MeasurementSystem MeasurementSystem { get; set; }
+
+        [JsonPropertyName("activities")]
         public string[] Activities { get; set; } = Array.Empty<string>();
     }
 
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum MeasurementSystem
     {
+        [JsonPropertyName("imperial")]
         Imperial,
+
+        [JsonPropertyName("metric")]
         Metric
     }
 }
